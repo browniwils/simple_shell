f42141373b3b32176e1bfc09defddb8ae88de78a
Browni Wilson <browniwils@gmail.com>
2023-04-27 04:40:33 +0000

    added swap char handler

---
+++ _swap_char_handler.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _swap_char_handler - swaps | and & for non-printed chars
+ * @input: input string
+ * @b: type of swap
+ * Return: swapped string
+ */
+
+char *_swap_char_handler(char *input, int b)
+{
+	int i;
+
+	if (b == 0)
+	{
+		for (i = 0; input[i]; i++)
+		{
+			if (input[i] == '|')
+			{
+				if (input[i + 1] != '|')
+					input[i] = 16;
+				else
+					i++;
+			}
+
+			if (input[i] == '&')
+			{
+				if (input[i + 1] != '&')
+					input[i] = 12;
+				else
+					i++;
+			}
+		}
+	}
+	else
+	{
+		for (i = 0; input[i]; i++)
+		{
+			input[i] = (input[i] == 16 ? '|' : input[i]);
+			input[i] = (input[i] == 12 ? '&' : input[i]);
+		}
+	}
+	return (input);
+}
+
